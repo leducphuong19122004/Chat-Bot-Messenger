45d82f8c35cd2ee6649171f0511e83e4f2d09482
Le_Duc_Phuong <phuongtroc2004@gmail.com>
2023-08-26 23:56:11 +0700

    fix error

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 import initRoute from './routes/index.js';
 import bodyParser from 'body-parser';
-import configViewEngine from './configs/viewEngine.js';
 
 
 const app = express();
@@ -11,7 +10,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 initRoute(app);
-configViewEngine(app);
 
 
 app.listen(5000, () => console.log('Express server is listening on port 5000'));
