edeebf70f836b274938b50817787d78d17e1e80e
Le_Duc_Phuong <phuongtroc2004@gmail.com>
2023-08-26 23:51:22 +0700

    delete ejs

--- configs/viewEngine.js
+++
@@ -1,8 +0,0 @@
-import path from "path";
-// View engines allow us to render web pages using template files. These templates are filled with actual data and served to the client.
-function configViewEngine(app) {
-    app.set("view engine", "ejs");
-    app.set("views", "views");
-}
-
-export default configViewEngine;
