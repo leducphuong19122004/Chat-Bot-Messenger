4165d564fcc5670689450c3b6478ad4477dd8ffd
Le_Duc_Phuong <phuongtroc2004@gmail.com>
2023-08-26 23:13:41 +0700

    fix error

--- configs/viewEngine.js
+++ configs/viewEngine.js
@@ -1,9 +1,8 @@
+import path from "path";
 // View engines allow us to render web pages using template files. These templates are filled with actual data and served to the client.
-import express from "express";
-
 function configViewEngine(app) {
     app.set("view engine", "ejs");
-    app.set("views", "./views");
+    app.set("views", "views");
 }
 
 export default configViewEngine;
