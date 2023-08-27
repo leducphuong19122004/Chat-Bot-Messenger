89245d1ea7fc0b02067e3ac1d2a3f706e76b5453
Le_Duc_Phuong <phuongtroc2004@gmail.com>
2023-08-27 12:59:25 +0700

    test

--- index.js
+++ index.js
@@ -11,5 +11,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 initRoute(app);
 
-
+// Handle other routes
+app.use((req, res) => {
+    res.status(404).send('Not Found');
+});
+  
 app.listen(5000, () => console.log('Express server is listening on port 5000'));
